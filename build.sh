237db857b40f5e31b2d9f36818122417b71948b3
egorov.andrey34 <egorov.andrey34@wb.ru>
2023-08-05 22:05:07 +0300

    HW6: Обновление docker до последней версии

--- build.sh
+++ build.sh
@@ -9,8 +9,20 @@ if [[ ! -d "$1" ]]; then
     exit 1
 fi
 
+if [[ ! -f /etc/apt/sources.list.d/docker.list ]]; then
+    apt update --quiet
+    apt install --quiet --yes ca-certificates curl gnupg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+    echo \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+fi
+
 apt update --quiet
-apt install --quiet --yes docker.io
+apt install --quiet --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 if [[ -n "$(docker ps --quiet --filter name="$1")" ]]; then
     docker stop "$1"
